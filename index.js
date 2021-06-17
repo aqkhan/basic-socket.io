427ac619feb3979b702df03e44170c1fc764f6a4
aqkhan <aqk.neo@gmail.com>
2021-06-17 22:14:25 +0500

    basic setup

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+    res.json({
+        "message": "success"
+    })
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+});
+
+server.listen(3000, () => {
+    console.log('listening on *:3000');
+});
